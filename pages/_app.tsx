7ac83522cfa0a0234800aa8f76ab14834bea50df
Hezekiah <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2022-03-21 22:41:11 -0400

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-PX2X5M3' })
+  }, [])  
   return <Component {...pageProps} />
 }
 
